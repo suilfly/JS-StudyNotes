b716600c063f98e327b1e5fb069d6020200807b9
李倩如 <liqianru@uino.com>
2023-01-03 17:17:58 +0800

    深拷贝v0.1

--- 练习项目/图片懒加载&预加载/图片懒加载&预加载.js
+++ 练习项目/图片懒加载&预加载/图片懒加载&预加载.js
@@ -1,3 +1,4 @@
+// 懒加载：先显示骨架屏，在图片请求回来后替换骨架屏的位置
 const imgData = [
   { img: './images/1.jpg', name: '图片1' },
   { img: './images/1.jpg', name: '图片2' },
@@ -40,9 +41,54 @@ const imgData = [
   function bindEvent() {
     const wrapper = document.querySelector('.wrapper')
     wrapper.addEventListener('scroll', (e) => {
-      console.log(e)
+      console.log(e.target.scrollTop)
+      const lists = [...document.getElementsByClassName('img-item')]
+      lists.forEach((list) => {
+        console.log(list)
+      })
+
     })
   }
 
   init()
 })();
+
+(function () {
+  const type = Object.prototype.toString.call
+
+  /**
+   * @description 对象深克隆
+   * @param target 被克隆的对象
+  */
+  function deepClone(target) {
+    const result = {}
+
+    for (key in target) {
+      // 如果复制的属性的类型不是对象或复制的属性的类型为数组，那么直接赋值就可以
+      if (typeof target[key] !== 'object' || type(target[key]) === '[object Array]') {
+        debugger
+        result[key] = target[key]
+      }
+      // 如果复制的属性的类型为对象，那么需要进入到每一层进行复制
+      if (type(target[key]) === '[object Object]') {
+        result[key] = deepClone(target[key])
+      }
+    }
+    return result
+  }
+
+})()
+
+
+let obj = {
+  a: 2,
+  b: {
+    c: 2,
+    d: {
+      f: 2,
+      fn: function () { },
+      arr: [1, 1, 1]
+    }
+  }
+}
+let res = deepClone(obj)
